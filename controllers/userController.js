08f5d8210b1edb6c5372a1d51bdc167d13d4a422
Mattthew Lusty <mattlusty123@gmail.com>
2022-01-21 12:26:52 +0000

    feat: add mongoose and db connection and user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,23 @@
 const { getPostData } = require("../utils/utils");
+const User = require("../models/user");
 
 async function createUser(req, res) {
   console.log("create user");
   try {
     var body = await getPostData(req);
     var object = JSON.parse(body);
-    var user = {
+
+    var userObject = {
       name: object.name,
-      email: undefined,
-      username: 007,
+      username: object.username,
+      email: object.email,
     };
 
-    // Pass to Model to implement writing to database
-    // var newUser = User.create(user);
+    // Call to Model to write to database collection
+    const userDocument = await User.create(userObject);
 
     res.writeHead(201, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(user));
+    res.end(JSON.stringify(userDocument));
   } catch (error) {
     console.log("Error: " + error);
   }
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const { createUser } = require("./controllers/userController.js");
 
-const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(
+    "mongodb+srv://mattlusty123:supersecretpassword@cluster0.4pa5p.mongodb.net/matt-app?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    console.log("db connect success");
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
+      console.log("Server is listening on PORT " + PORT);
+    });
+  });
+
 var server = http.createServer((req, res) => {
   console.log("Request received");
-  // var method = req.method;
-  // var url = req.url;
-  // console.log("the url was " + url);
   if (req.url === "/api/users" && req.method === "POST") {
     console.log("route create user");
     createUser(req, res);
   }
-});
-
-server.listen(PORT, () => {
-  console.log("Server is listening on PORT " + PORT);
 });
