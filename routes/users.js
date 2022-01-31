2e5ede7b428c28b13e307afdeb3ac05ef48c6fe1
Mattthew Lusty <mattlusty123@gmail.com>
2022-01-31 21:14:37 +0000

    feat: add express library and some express logic

---
+++ routes/users.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const userController = require("../controllers/user.js");
+
+const portUsersRouter = express.Router();
+
+portUsersRouter
+  .route("/")
+  .get((req, res) => {
+    console.log("GET /portfolio/users");
+    res.send("GET /portfolio/users");
+  })
+  .post((req, res) => {
+    console.log("POST = /portfolio/users");
+    // res.send("POST = /portfolio/users");
+
+    userController.createUser(req, res);
+  });
+
+portUsersRouter
+  .route("/:id")
+  .get((req, res) => {
+    console.log("xroute = /portfolio/users/:id");
+    res.send("xroute = /portfolio/users/:id");
+  })
+  .delete((req, res) => {
+    console.log("xroute = /portfolio/users/:id");
+    res.send("xroute = /portfolio/users/:id");
+  });
+
+module.exports = portUsersRouter;
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const http = require("http");
 const mongoose = require("mongoose");
+const express = require("express");
 
+const app = express();
+
+// connect to db and start server app listening
 mongoose
   .connect(
     "mongodb+srv://mattlusty123:supersecretpassword@cluster0.4pa5p.mongodb.net/matt-app?retryWrites=true&w=majority"
   )
   .then((result) => {
     console.log("db connect success");
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () => {
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
       console.log("Server is listening on PORT " + PORT);
     });
   });
 
-var server = http.createServer((req, res) => {
-  req.url === "/portfolio/users" ? require("./routes/portfolio/user")(req, res) : "";
-});
+//define routes
+app.use("/portfolio/users", require("./routes/users"));
